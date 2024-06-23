20ca74f8ea9d19d523bb9e71ecbf44952c12e20a
octospacc <octo@tutamail.com>
2024-06-24 00:10:38 +0200

    Auto-Backup $'Sun Jun 23 2024 23:53:36 GMT+0200 (Central European Summer Time)'

--- filter-fix-posts.sh
+++ filter-fix-posts.sh
@@ -15,24 +15,23 @@ do
 		if [ -z "${isAuthorCorrect}" ] || [ -z "${isPublished}" ]
 		then rm -v {}
 		else
-			set $(head -n${yamlEnd} {} | grep "post_date: "); date="$2 $3"; dateSlug="$(echo "$2-$3" | sed "s/:/-/g")"
-			set $(head -n${yamlEnd} {} | grep "post_name: "); slug="$2"
+			head -n${yamlEnd} {} > {}.yaml.tmp
+			sed -i -E sed -E ":a; N; $!ba; s/(:\s*)>\n\s*/\1/; s/>\n\s*/ /g; s/\n\s+/ /g" {}.yaml.tmp
+			set $(head -n${yamlEnd} {} | grep "post_date: "); date="$2 $3"; day="$2" #dateSlug="$(echo "$2-$3" | sed "s/:/-/g")"
+			#set $(head -n${yamlEnd} {} | grep "post_name: "); slug="$(echo "$2" | "s/\"//g")"
+			set $(grep "post_name: " {}.yaml.tmp); slug="$(echo "$2" | "s/\"//g")"
 			head -n${yamlEnd} {} | head -n-1 > {}.tmp
-			set $(head -n${yamlEnd} {} | grep "post_title: "); shift; echo "title: $@" >> {}.tmp
+			#set $(head -n${yamlEnd} {} | grep "post_title: "); shift; echo "title: $@" >> {}.tmp
+			set $(grep "post_title: " {}.yaml.tmp); shift; echo "title: $@" >> {}.tmp
 			echo "date: ${date}" >> {}.tmp
 			echo "canonical_url: $(head -n${yamlEnd} {} | grep "  https://octospacc.altervista.org/")" >> {}.tmp
 			tail -n+${yamlEnd} {} >> {}.tmp
 			mv {}.tmp {}
+			rm {}.yaml.tmp
 			sed -i "s|src=\"https://octospacc.altervista.org/wp-content/uploads/|src=\"{{site.cdnurl}}/assets/uploads/|g" {}
-			mv {} "_posts/${dateSlug}-${slug}.md"
+			sed -i "s|<a href=\"https://octospacc.altervista.org/|<a href=\"|g" {}
+			#sed -E "s/<a[^>]*href=\"https:\/\/octospacc.altervista.org\/([^\"]*)[^>]*>([^<]*)<\/a>/[\2](\1.md)/g" | sed -E ":a; s/(\[[^]]*\]\([^)]*)\//\1-/; ta" | cat > {}
+			mv {} "_posts/${day}-${slug}.md"
 		fi
 	' \; || true 
 done
-
-#if [ -z "$(head -n${yamlEnd} {} | grep "canonical_url: ")" ]; ...
-
-#elif [ -z "$(head -n${yamlEnd} {} | grep "post_fixed_placeholder: true")" ]
-#	...
-#	echo "date: ${date}" >> {}.tmp
-#	...
-#	echo "post_fixed_placeholder: true" >> {}.tmp
